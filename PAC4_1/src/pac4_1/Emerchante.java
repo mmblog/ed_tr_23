d40a0e6d64b8cc7af89a3dbbebf5a3ba1121311f
admin <admin@DESKTOP-UIL5VL6>
2023-02-26 13:24:07 +0100

    Modificacion de CGuerrero a EMerchante

--- PAC4_1/src/pac4_1/Emerchante.java
+++ PAC4_1/src/pac4_1/Emerchante.java
@@ -8,7 +8,7 @@ public class Emerchante {
 	/////////////////////////////////CONSTRUCTOR/////////////////////////////////
 	public Emerchante(String nombre, String apellido1, int edad) {
 		this.nombre="Enrique";
-		this.apellido1="Merchante Moyano"; //Añado segundo apellido
+		this.apellido1="Merchante Moyano"; //Añado segundo apellido.
 		this.edad=28;
 	}
 	
