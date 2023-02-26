a8a97d5364ca78b633f30c6eb3c66f9a974b3202
Emermoy <124273154+Emermoy@users.noreply.github.com>
2023-02-26 12:26:34 +0100

    Implementando (Emerchante)

---
+++ PAC4_1/src/pac4_1/Emerchante.java
@@ -0,0 +1,32 @@
+package pac4_1;
+
+public class Emerchante {
+	public String nombre;
+	public String apellido1;
+	public int edad;
+	
+	/////////////////////////////////CONSTRUCTOR/////////////////////////////////
+	public Emerchante(String nombre, String apellido1, int edad) {
+		this.nombre="Enrique";
+		this.apellido1="Merchante";
+		this.edad=28;
+	}
+	
+	
+	//////////////////////////////////METODOS///////////////////////////////////
+	public void borrarDatos() {
+		this.nombre="";
+		this.apellido1="";
+		this.edad=0;
+	}
+		
+	public void cumpleanyo() {
+		this.edad+=1;
+	}
+		
+	@Override
+	public String toString(){ 
+		return "\n"+nombre+" "+apellido1+" ("+edad+")";    
+	}  	
+	
+}
