9add74298e010237a1c6f46dfd0d626e468afd36
Manu <Manu@DESKTOP-MOOSU1U>
2023-02-27 16:36:28 +0100

    Implementando y Manuel

---
+++ PAC_4__1/src/actividad/MIrizo.java
@@ -0,0 +1,12 @@
+package actividad;
+
+public class MIrizo {
+	  public static void main(String[] args) {
+          // TODO Auto-generated method stub
+          System.out.print("Manuel:");
+
+          System.out.print ("Irizo:");
+
+          System.out.print ("21:");
+}
+}
