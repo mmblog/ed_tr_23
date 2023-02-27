47a62c66f73727d3f8442fd2add7272f13af966c
mario4veinte <mario4veinte@gmail.com>
2023-02-27 17:00:16 +0100

    Final

--- PAC_4__1/src/actividad/main.java
+++ PAC_4__1/src/actividad/main.java
@@ -5,6 +5,30 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Datos Fran 
+		
+        System.out.print("Francisco:");
+
+        System.out.print ("Marín:");
+
+        System.out.print ("18:");
+
+        // Datos Mario
+        
+        System.out.print("Mario:");
+
+        System.out.print ("Bernal:");
+
+        System.out.print ("21:");
+        
+        // Datos Manu
+        
+        System.out.print("Manuel:");
+
+        System.out.print ("Irizo:");
+
+        System.out.print ("21:");
+
 	}
 
 }
