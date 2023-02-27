892222b609d577ea6bd531fda649363619f9fc1a
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2023-02-27 15:45:58 +0100

    Implementando (Rubén Arcos)

--- Clase_Git_4_1/src/clase/main.java
+++ Clase_Git_4_1/src/clase/main.java
@@ -2,4 +2,37 @@ package clase;
 
 public class main {
 
+    public main(String[] args) {
+
+    	// Rubén
+    	
+    	System.out.print("Nombre: Rubén Arcos");
+    	
+    	System.out.print("Edad: 23");
+    	
+    	System.out.print("Fecha de nacimiento: 07/07/1999");
+    	
+    	System.out.print("Curso: 1ºDam");
+    	
+    	// Patricia
+    	
+    	System.out.print("Nombre: ");
+    	
+    	System.out.print("Edad: ");
+    	
+    	System.out.print("Fecha de nacimiento: ");
+    	
+    	System.out.print("Curso: 1ºDam");
+   
+    	// Jose Ramón
+    	
+    	System.out.print("Nombre: ");
+    	
+    	System.out.print("Edad: ");
+    	
+    	System.out.print("Fecha de nacimiento: ");
+    	
+    	System.out.print("Curso: 1ºDam");
+    	
+    }
 }
