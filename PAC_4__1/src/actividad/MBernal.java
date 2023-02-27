311fd985a3a35bfdb0ddab559c537275b34b49a2
mario4veinte <mario4veinte@gmail.com>
2023-02-27 16:31:25 +0100

    Implementando (Mario)

---
+++ PAC_4__1/src/actividad/MBernal.java
@@ -0,0 +1,13 @@
+package actividad;
+
+public class MBernal {
+
+	  public static void main(String[] args) {
+          // TODO Auto-generated method stub
+          System.out.print("Mario:");
+
+          System.out.print ("Bernal:");
+
+          System.out.print ("21:");
+}
+}
