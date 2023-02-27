ad3d01f82cd176bd6f2cc684ce4db01be3df20f4
FranMaRRo <124273007+FranMaRRo@users.noreply.github.com>
2023-02-27 16:16:42 +0100

    Implementando (francisco)

--- PAC_4__1/src/actividad/FranMarin.java
+++ PAC_4__1/src/actividad/FranMarin.java
@@ -1,5 +1,12 @@
 package actividad;
 
 public class FranMarin {
+	  public static void main(String[] args) {
+          // TODO Auto-generated method stub
+          System.out.print("Francisco:");
 
+          System.out.print ("Marín:");
+
+          System.out.print ("18:");
+}
 }
