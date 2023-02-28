cddaf82f31e1d6495b6c032b20f7670678f3bdf6
DavMedRui <124273167+DavMedRui@users.noreply.github.com>
2023-02-28 15:14:52 +0100

    Primer Commit

---
+++ Practica_4_1/src/DMedina.java
@@ -0,0 +1,25 @@
+public class DMedina {
+    String apellidos;
+    String nombre;
+
+    public DMedina(String apellidos, String nombre) {
+        this.apellidos = "Medina";
+        this.nombre = "David";
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
