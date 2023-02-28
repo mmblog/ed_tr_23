f080e4dc151ca7aabaef61509035b7dc7384aa58
DavidChaveli <124272965+DavidChaveli@users.noreply.github.com>
2023-02-28 17:31:35 +0100

    Final

---
+++ Practica_4_1/src/casadoDavid.java
@@ -0,0 +1,25 @@
+public class casadoDavid {
+    String nombre;
+    String apellidos;
+
+    public casadoDavid(String nombre, String apellidos) {
+        this.nombre = "David";
+        this.apellidos = "casado";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
