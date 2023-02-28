6c711ac453a15d3fc66187c7dfd641af92114da3
fcomargar <franciscomartinezgarcia@iestorredelrey.es>
2023-02-28 15:49:26 +0100

    "Implementando"

---
+++ Practica_4_1/src/fmartinez.java
@@ -0,0 +1,26 @@
+public class fmartinez {
+    String nombre;
+    String apellido;
+
+    public fmartinez(String nombre, String apellido) {
+        this.nombre = "Francisco";
+        this.apellido = "Martinez";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
+
